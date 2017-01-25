4bea8ca4e3f65df9c97370a379b8b6a0d3bba174
i.senuna <i.senuna@gmail.com>
2017-01-25 23:54:48 +0000

    Data structures start

---
+++ src/dataStructures/ArraysDS.java
@@ -0,0 +1,17 @@
+package dataStructures;
+
+import java.util.Scanner;
+
+/**
+ * Created by senuna on 25.01.17.
+ */
+public class ArraysDS {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+    }
+}
